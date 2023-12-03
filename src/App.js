d689c384624a83fc6c83e5cde03ead7a150a2f15
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-12-02 19:57:09 -0800

    working again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import user from "./db.js";
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <div className="revenue">{user}</div>
     </div>
   );
 }
