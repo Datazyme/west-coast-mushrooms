5ebaa9f68d48c8d77a96e81da6609ee0c5902a51
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-06-23 18:11:00 -0700

    removing original files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-  <h1>Hello World</h1>
+    <h1>Hello World</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
