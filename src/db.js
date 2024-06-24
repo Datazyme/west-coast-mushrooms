e83349344411bcc755cae2443985ac23bea89702
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-06-23 17:30:36 -0700

    changing name

--- src/db.js
+++ src/db.js
@@ -1,1 +1,17 @@
+// server.js
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
+// Replace these with your PostgreSQL connection details
+
+const config = {
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  port: process.env.POSTGRES_PORT
+};
+
+const pool = new Pool(config);
+
+module.exports = pool;
