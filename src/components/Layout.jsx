2f73144156e08b5a500d469498ffdbc521450e19
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-06-23 19:36:42 -0700

    with layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = ({children}) => {
+const Layout = () => {
   return (
     <>
         <Header></Header>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
