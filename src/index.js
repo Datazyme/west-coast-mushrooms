f424b9f9560ecffda974dc1117645512bb0558a0
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-06-23 20:07:28 -0700

    with routes to pages

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import ErrorPage from './pages/ErrorPage';
+import MushroomDetails from './pages/MushroomDetails'
+import MushroomFamilies from './pages/MushroomFamilies'
+import MushroomEdibility from './pages/MushroomEdibility'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout></Layout>,
     errorElement: <ErrorPage></ErrorPage>,
+    //used in Outlet in layout
+    children: [
+      {index: true, element: <Home></Home>},
+      {path: 'mushrooms/:id', element: <MushroomDetails></MushroomDetails>},
+      {path: 'mushrooms/families/:family', element: <MushroomFamilies></MushroomFamilies>},
+      {path: 'mushrooms/edibility/id', element: <MushroomEdibility></MushroomEdibility>}
+    ]
   }
 ])
 
