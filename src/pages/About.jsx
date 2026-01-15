a6b5e2781fb5da30dcbd4a3b4d3d317bd0db2008
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mac-mini.local>
2026-01-14 20:00:41 -0800

    changing about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <article className='about'>
-        <h3>Mushroom picking is something that I did with my family in childhood as this is common in my culture. It was fun finding them in the woods, like an easter egg hunt. I helped my mother prepare them too, learning about how different mushrooms are more suitable for some cooking methods but not for others. As an adult I have come to the decision that I would rather photograph and learn about how they survive, grow and contribute to the surrounging environment. I find this is more rewarding as it not destructive and a lot less work. Mushrooms are so much more beautiful when they are left to be.
+        <h3>
         </h3>
     </article>
   )
